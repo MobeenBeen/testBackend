31421043f3a71c36f4f7a5b2a4b0dc553b830aa6
MobeenBeen <mobeenislam7861@gmail.com>
2023-12-24 18:30:32 +0500

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-require('dotenv').config()
-const express = require('express')
+// require('dotenv').config()
+// const express = require('express')
+import dotenv from 'dotenv';
+import express from 'express'; 
 const app = express()
 const port = 3000
 
@@ -7,10 +9,38 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/module',(req,res)=>{
-    res.send('this is customized response')
+app.get('/joke',(req,res)=>{
+    const joke=[
+        {
+          "id": 1,
+          "title": "joke 1",
+          "content": "This is the first joke."
+        },
+        {
+          "id": 2,
+          "title": "joke 2",
+          "content": "Here's another joke for you."
+        },
+        {
+          "id": 3,
+          "title": "joke 3",
+          "content": "Why did the chicken cross the road? To get to the other side!"
+        },
+        {
+          "id": 4,
+          "title": "joke 4",
+          "content": "Knock, knock. Who's there? Orange. Orange who? Orange you glad I didn't say banana?"
+        },
+        {
+          "id": 5,
+          "title": "joke 5",
+          "content": "Last but not least, this is the fifth joke."
+        }
+      ]
+      
+    res.send(joke)
 })
 
-app.listen(process.env.port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
